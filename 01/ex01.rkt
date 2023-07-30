2f5b94314da50f88da1eeb798560c75cdcddf255
teeaychem <ben@sparkes.uk.net>
2023-07-30 14:47:45 +0100

    Ex 1.35

--- 01/ex01.rkt
+++ 01/ex01.rkt
@@ -1100,3 +1100,37 @@ With f labelled
 (f1 f2) - > (f2 2) -> (2 2)
 But, 2 isn't a procedure.
 |#
+
+
+;; Ex. 1.35
+
+#|
+From p. 38 we have the golden ratio is (1 + sqrt(5))/2.
+Let φ = (1 + sqrt(5))/2.
+And, we know φ^2 = φ + 1.
+
+To figure out how to express φ as a fixed point of x, we'll work through x = φ until we have a non-trivial transformation of x on the rhs.
+So:
+
+x = φ
+x^2 = φ^2 \ Algebra
+x^2 = φ + 1 \ Given
+x^2 = x + 1 \ Initial equality.
+x = (x + 1)/x \ Algebra
+x = (x/x + 1/x) \ Algebra
+x = (1 + 1/x) \ Algebra
+|#
+
+
+(define (fixed-point f first-guess tolerance)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(display "φ is roughly: ")
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0 .0000001)
