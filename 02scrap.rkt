9950c614f0e5a9a100ec3f449ad421fa59bc544c
teeaychem <ben@sparkes.uk.net>
2023-07-31 22:56:52 +0100

    Files renamed and subfolders removed.

---
+++ 02scrap.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+
+
+; (define x (cons 1 2 3))
+#|
+cons as a two place operator seems odd.
+Esp. given + takes an arbirary number of places.
+Though, I can see this in terms of a linked list.
+Where, car and cdr are value and point (or vice-versa).
+Still, as far as the textbook goes, starting with the abstraction of lists and then seeing the implementation is what I expected.
+|#
