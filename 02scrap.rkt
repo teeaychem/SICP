169bb6771a7a36d6725b840952e1b9469d8cc0f9
teeaychem <ben@sparkes.uk.net>
2023-08-08 17:26:56 +0100

    Attempt at displaying binary trees.

--- 02scrap.rkt
+++ 02scrap.rkt
@@ -24,3 +24,44 @@ Okay, so here we quote a list of things.
 
 '(a b c) = (list 'a 'b 'c)
 |#
+
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set)) (element-of-set? x (left-branch set)))
+        (else (element-of-set? x (right-branch set)))
+        )
+  )
+
+(define (adjoin-set x set)
+  (cond
+    ((null? set) (make-tree x nil nil))
+    ((= x (entry set)) set)
+    ((< x (entry set)) (make-tree (entry set) (adjoin-set x (left-branch set)) (right-branch set)))
+    ((> x (entry set)) (make-tree (entry set) (left-branch set) (adjoin-set x (right-branch set))))
+    )
+  )
+
+
+#|
+This is v. lazy with append.
+But, it works.
+|#
+(define (display-set set)
+  (if (null? (entry set))
+      nil
+      (let ((leftBranch (if (null? (left-branch set)) nil (display-set (left-branch set))))
+            (rightBranch (if (null? (right-branch set)) nil (display-set (right-branch set)))))
+        (append leftBranch (cons (entry set) rightBranch))
+  )))
+
+
+(display-set (adjoin-set 1 (adjoin-set 5 (adjoin-set 0 (adjoin-set 2 (adjoin-set 25 nil))))))
+(display-set (adjoin-set 25 (adjoin-set 2 (adjoin-set 0 (adjoin-set 5 (adjoin-set 1 nil))))))
