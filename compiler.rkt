ceda8dab5ea1da01d375ca8769555ca064cbe125
teeaychem <ben@sparkes.uk.net>
2023-09-16 10:36:25 +0100

    No real change

--- compiler.rkt
+++ compiler.rkt
@@ -581,13 +581,13 @@
 ;;  'val
 ;;  'next)
 
-;; (compile
-;;  '(define (factorial n)
-;;   (define (iter product counter)
-;;     (if (> counter n)
-;;         product
-;;         (iter (* counter product)
-;;               (+ counter 1))))
-;;   (iter 1 1))
-;;  'val
-;;  'next)
+(compile
+ '(define (factorial n)
+    (define (iter product counter)
+      (if (> counter n)
+          product
+          (iter (* counter product)
+                (+ counter 1))))
+    (iter 1 1))
+ 'val
+ 'next)
